4dcb3d3add54ad61f4852ee66cbe598001ce1395
amotovilov <wispard@google.com>
2020-07-29 16:26:33 +0300

    todolistV2 fix

--- To Do List V2/script.js
+++ To Do List V2/script.js
@@ -1,22 +1,55 @@
-function fetchData() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            const html = data.map(task => {
-                return `<div class="task ${task.completed}">Title: ${task.title}.</div>`;
-            }).join('');
-            document.getElementById('app').insertAdjacentHTML('afterbegin', html);
-            document.getElementById('app').addEventListener('click', toggleTaskState);
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+const APP = document.getElementById('app');
+const TASK_ITEM_CLASS = 'task';
+const DELETE_ITEM_CLASS = 'delete-btn';
+const TASK_COMPLETE_CLASS = 'true';
+let todosList = [];
 
-            function toggleTaskState(e) {
-                if (e.target.classList.contains('task'))
-                    e.target.classList.toggle('true');
-            }
-
-        })
-};
+APP.addEventListener('click', onElementClick);
 
 fetchData();
+
+
+function fetchData() {
+    fetch(TODOS_URL)
+        .then((response) => response.json())
+        .then(setUpData);
+}
+
+
+function setUpData(data) {
+    console.log(data);
+    todosList = data;
+    const html = data.map(task => {
+        return `<div class="task ${task.completed}" data-task-id="${task.id}">Title: ${task.title}. <span class="delete-btn">⌫</span></div>`;
+    }).join('');
+    renderData(html);
+
+};
+
+function renderData(html) {
+    APP.insertAdjacentHTML('afterbegin', html);
+}
+
+function onElementClick(e) {
+    switch (true) {
+        case e.target.classList.contains(TASK_ITEM_CLASS):
+            toggleTaskState(e.target);
+            break;
+        case e.target.classList.contains(DELETE_ITEM_CLASS):
+            deleteTask(e.target.parentElement);
+    }
+}
+
+function toggleTaskState(el) {
+    el.classList.toggle(TASK_COMPLETE_CLASS);
+}
+
+function deleteTask(el) {
+    const taskId = el.dataset.taskId;
+    console.log(taskId);
+
+    todosList = todosList.filter((task) => task.id != taskId);
+
+    el.remove();
+}
